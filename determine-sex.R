48434623111a777ce701cb284dfde20ec0409b5c
Ankit Hinsu <118174127+AHinsu@users.noreply.github.com>
2023-03-02 11:18:55 +0000

    Updated R script to remove space in name & include all columns

--- determine-sex.R
+++ determine-sex.R
@@ -3,6 +3,8 @@
 set.seed(100)
 #Set working directory
 setwd("C:/Users/ahinsu/Downloads/skim-data-pilot")
+library("tidyverse")
+
 
 #Chage the working directory again
 filenames <- list.files(path = "C:/Users/ahinsu/Downloads/skim-data-pilot", pattern = ".bam.stats", full.names = TRUE)
@@ -18,13 +20,12 @@ new.df <- merge(x = temp, y = my.df1, by = "V1")
 colnames(new.df)[1:2] <- c("Scaffold-name", "Scaffold-length")
 write.table(x = new.df, file = "per-scaffold-read-mappings.txt", quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
 
-new.df1 <- new.df[new.df$`Scaffold name` %in% c("chrW", "chrZ"),2:ncol(new.df)] %>% t()
+new.df1 <- new.df[new.df$Scaffold-name %in% c("chrW", "chrZ"),2:ncol(new.df)] %>% t()
 colnames(new.df1) <- c("chrW", "chrZ")
 new.df1 <- as.data.frame(new.df1)
 new.df1$proporiton <- new.df1$chrW/new.df1$chrZ
-threshold <- new.df1["Scaffold-length","proporiton"] #This should be something around 0.082
+threshold <- new.df1["Scaffold-length","proporiton"]
 new.df1 <- new.df1[-1,]
 new.df1$sex <- ifelse(new.df1$proporiton < threshold, "Male", ifelse(new.df1$proporiton > threshold, "Female", "Same"))
-new.df1$Sample <- rownames(new.df1)
-new.df1 %>% select(Sample, chrW, chrz, proportion, sex)
-write.table(x = new.df1, file = "Estimated-sex.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(x = new.df1, file = "Estimated-sex.txt", quote = FALSE, sep = "\t", row.names = TRUE, col.names = TRUE)
+
