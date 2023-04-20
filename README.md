602c935b4c07ff32f3ccc6355d1bdba1d6ffb218
nylander <j.a.a.nylander@gmail.com>
2023-04-20 13:26:05 +0200

    Add commandArgs and correct bug when calling table column

--- README.md
+++ README.md
@@ -1,17 +1,37 @@
 # Chicken-sex-determination
-This contains the scripts to determine the sex of chicken from the WGS data. This requires an indexed bam file.
 
+This contains the scripts to determine the sex of chicken from the WGS data.
+This requires an indexed bam file.
 
-Requirements:
-1. An indexed bam files per samples. To check if you  bam files are indexed, look for file with same name as bam file and with extension .bai. The scripts could also run with non-indexed bam files, but it will take more time to complete.
-2. Samtools installed in the system and should bea accessed globally.
-3. R installed with packages: dplyr and tidyr
+## Requirements
 
+1. An indexed bam file per sample. To check if your bam files are indexed,
+   look for file with same name as bam file and with extension .bai. The
+   scripts could also run with non-indexed bam files, but it will take more
+   time to complete.
+2. Samtools installed (as `samtools`) in the user PATH.
+3. R and Rscript installed with packages dplyr and tidyr.
 
-Usage:
-1. Run the shell script 'calculate-stats.sh' by command "./calculate-stats.sh". This will generated .stats files for each samples.
-2. Run the R script 'determine-sex.R'. This will generate a combined per scaffold statistics for all samples and an estimated-sex.txt file.
+## Usage
 
+1. Run the shell script 'calculate-stats.sh' by command `./calculate-stats.sh`.
+   This will generate a .stats file for each sample.
+   For more control, one may manually modify and run these commands:
 
-Note:
-1. It is assumed that chromosomes are named as "chr1, chr2,..." and sex chromosomes are "chrW" and "chrZ". These sex chromosomes can be modified for other organisms as well. This could also be checked from the generated per scaffold statistics file.
+        $ mkdir -p bamstats-folder
+        $ bampaths='/path/to/folder/with/bamfiles/and/bamindexes'
+        $ for f in $(find "${bampath}" -name '*.bam') ; do
+            g=$(basename "$f");
+            samtools idxstats --threads 8 "$f" > bamstats-folder/"$g".stats
+          done
+
+2. Run the R script 'determine-sex.R': `./determine-sex.R bamstats-folder`.
+   This will generate a combined per scaffold statistics for all samples and an
+   Estimated-sex.txt file.
+
+## Note
+
+1. It is assumed that chromosomes are named as "chr1, chr2,..." and sex
+   chromosomes are "chrW" and "chrZ". These sex chromosomes can be modified for
+   other organisms as well. This could also be checked from the generated per
+   scaffold statistics file.
--- determine-sex.R
+++ determine-sex.R
@@ -1,35 +1,91 @@
-#!/usr/bin/env Rscript --vanilla
+#!/usr/bin/env -S Rscript --vanilla
 
-set.seed(100)
-#Set working directory
-setwd("C:/Users/ahinsu/Downloads/skim-data-pilot")
-library("tidyverse")
+# Description: Assign sex from mapping ratio against W and Z chromosomes
+#
+# Usage: ./determine-sex.R folder-with-bamstats-files
+#
+# Input: .bam.stats files. Should be the output from (example):
+#
+#     mkdir -p folder-with-bamstats-files
+#     for f in $(find /path/to/folder/with/bamfiles/and/bamindexes); do
+#       g=$(basename "$f");
+#       samtools idxstats "$f" > folder-with-bamstats-files/"$g".stats
+#     done
+#
+# Output: per-scaffold-read-mappings.tsv
+#         Estimated-sex.tsv
 
+# Expect one argument: folder name or "-h"/"--help"
+args <- commandArgs(TRUE)
 
-#Chage the working directory again
-filenames <- list.files(path = "C:/Users/ahinsu/Downloads/skim-data-pilot", pattern = ".bam.stats", full.names = TRUE)
-# Read in the data for all the files, and create a new df with sample-name/filename as extra column.
+if (length(args) == 0) {
+    stop("Error: No input path to .bam.stats files", call. = FALSE)
+}
+
+if ("-h" %in% args || "--help" %in% args) {
+    cat("Usage: determine-sex.R /path/to/bam.stats/folder", "\n")
+    quit(save = "no")
+}
+
+bamstatspath <- args[1]
+cat(paste0("Reading bam.stats files in folder ", bamstatspath, "\n"))
+
+suppressPackageStartupMessages({
+  library("dplyr")
+  library("tidyr")
+})
+
+# Read in the data for all the files, and create a new df with
+# sample-name/filename as extra column.
+filenames <- list.files(path = bamstatspath, pattern = ".bam.stats",
+                        full.names = TRUE)
+
 my.df <- do.call(rbind,
                  lapply(filenames, function(x) 
-                   cbind(read.delim(x, stringsAsFactors = FALSE, header = FALSE), 
+                   cbind(read.delim(x, stringsAsFactors = FALSE, header = FALSE),
                          name = tools::file_path_sans_ext(basename(x)))))
+
 my.df$name <- as.character(my.df$name)
 
-# Pivot the DF & modify to get per sample read mappings for each reference sequence which is then outputted as well.                       
-my.df1 <- my.df %>% select(V1, V3, name) %>% tidyr::spread(key = "name", value = "V3", fill = 0)
-temp <- my.df %>% select(V1, V2) %>% distinct(V1, V2)
+# Pivot the DF & modify to get per sample read mappings for each reference
+# sequence which is then outputted as well.
+my.df1 <- my.df %>%
+    select(V1, V3, name) %>%
+    tidyr::spread(key = "name", value = "V3", fill = 0)
+
+temp <- my.df %>%
+    select(V1, V2) %>%
+    distinct(V1, V2)
+
 new.df <- merge(x = temp, y = my.df1, by = "V1")
+
 colnames(new.df)[1:2] <- c("Scaffold-name", "Scaffold-length")
-write.table(x = new.df, file = "per-scaffold-read-mappings.txt", quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
 
-# Extract chrW and chrZ in the new df & calculate the ratio of reads in W/reads in Z. Also calucalte the ratio of lengths of chrW/chrZ which is threshold for decision.
-new.df1 <- new.df[new.df$Scaffold-name %in% c("chrW", "chrZ"),2:ncol(new.df)] %>% t()
+write.table(x = new.df, file = "per-scaffold-read-mappings.txt",
+            quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+
+# Extract chrW and chrZ in the new df and calculate the ratio of reads in
+# W/reads in Z. Also calcualte the ratio of lengths of chrW/chrZ which is the
+# threshold for decision.
+new.df1 <- new.df[new.df$'Scaffold-name' %in%
+                  c("chrW", "chrZ"), 2:ncol(new.df)] %>% t()
+
 colnames(new.df1) <- c("chrW", "chrZ")
-new.df1 <- as.data.frame(new.df1)
-new.df1$proporiton <- new.df1$chrW/new.df1$chrZ
-threshold <- new.df1["Scaffold-length","proporiton"]
-new.df1 <- new.df1[-1,]
-# Based on comparison with threshold, decide for each sample if it is male of female and output to file
-new.df1$sex <- ifelse(new.df1$proporiton < threshold, "Male", ifelse(new.df1$proporiton > threshold, "Female", "Same"))
-write.table(x = new.df1, file = "Estimated-sex.txt", quote = FALSE, sep = "\t", row.names = TRUE, col.names = TRUE)
 
+new.df1 <- as.data.frame(new.df1)
+
+new.df1$proportion <- new.df1$chrW/new.df1$chrZ
+
+threshold <- new.df1["Scaffold-length", "proportion"]
+
+new.df1 <- new.df1[-1, ]
+
+# Based on comparison with threshold, decide for each sample if it is male of
+# female and output to file
+new.df1$sex <- ifelse(new.df1$proportion < threshold, "Male",
+                      ifelse(new.df1$proportion > threshold, "Female", "Same"))
+
+write.table(x = new.df1, file = "Estimated-sex.txt",
+            quote = FALSE, sep = "\t", row.names = TRUE, col.names = TRUE)
+
+cat("End of script", "\n")
